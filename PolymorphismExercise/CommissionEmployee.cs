cbfec55b2872aa208bfe63c61b947e21a63cd710
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-22 11:42:01 -0500

    comm emp class

---
+++ PolymorphismExercise/CommissionEmployee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismExercise
+{
+    public class CommissionEmployee : Employee
+    {
+        public double GrossSales { get; set; }
+        public double CommissionRate { get; set; }
+
+        public CommissionEmployee(int id, string name, double grossSales, double commissionRate) : base(id, name)
+        {
+            GrossSales = grossSales;
+            CommissionRate = commissionRate;
+        }
+
+        public override double Earnings()
+        {
+            return GrossSales * CommissionRate;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"Gross Sales: {GrossSales:C}\nCommission Rate: {CommissionRate:P}\n";
+        }
+    }
+}
