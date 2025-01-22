3ff17abad3f8c71c5f4ea2eefbc697bdf924730a
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-22 10:59:39 -0500

    Add project files.

---
+++ W25Week3IntroToPolymorphism/BaseClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W25Week3IntroToPolymorphism
+{
+    public class BaseClass
+    {
+        public virtual void Show()
+        {
+            Console.WriteLine("Show from Base class");
+        }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Show from Derived class");
+        }
+    }
+}
---
+++ W25Week3IntroToPolymorphism/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace W25Week3IntroToPolymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add(2, 3);
+            
+            //int[] arr = { 2, 3, 4 };
+            //PrintArray(arr);
+
+
+
+            // method overriding
+            BaseClass objBase = new BaseClass();
+            objBase.Show();
+
+            //DerivedClass objDerived = new DerivedClass();
+            //objDerived.Show();
+
+            objBase = new DerivedClass();
+            objBase.Show();
+
+
+            //objDerived = new BaseClass();
+        }
+
+
+
+
+        // method overloading
+        static void PrintArray(int[] a)
+        {
+            // method body
+        }
+
+        //static void PrintArray(params int[] a)
+        //{
+        //    // method body
+        //}
+
+
+        static void Add(int n1, int n2)
+        {
+            int sum = n1 + n2;
+        }
+
+        //static int Add(int n1, int n2)
+        //{
+        //    return n1 + n2;
+        //}
+
+        static void Add(string n1, string n2)
+        {
+            string sum = n1 + n2;
+        }
+
+        static void Add(int n1, int n2, int n3)
+        {
+            int sum = n1 + n2 + n3;
+        }
+
+        static void Add(int n1, int n2, ref int n3)
+        {
+            int sum = n1 + n2 + n3;
+        }
+
+        
+    }
+}
